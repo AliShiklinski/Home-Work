7f66502b6df44c124e3055076d621c4dfc2bdbd3
AliShiklinski <169164888+AliShiklinski@users.noreply.github.com>
2024-05-21 23:12:20 +0400

    Add files via upload

---
+++ Task 2 05.21.2024/Task 2 05.21.2024/Class2.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+
+namespace Task_2_05._21._2024
+{
+    internal class Car
+    {
+        
+
+        public string Mercedes;
+        public string W210;
+        public double CurrentFuel;
+        public double FuelFor1Km;
+        public double Millage;
+
+        public Car(string brend, string model, double currentFuel, double fuelFor1Km)
+        {
+            Mercedes = brend;
+            W210 = model;
+            CurrentFuel = currentFuel;
+            FuelFor1Km = fuelFor1Km;
+            Millage = 0;
+        }
+
+        public void Drive(int km)
+        {
+            double requiredFuel = km * FuelFor1Km;
+
+            if (CurrentFuel >= requiredFuel)
+            {
+                Millage += km;
+                CurrentFuel -= requiredFuel;
+                Console.WriteLine($"{km} have traveled kilometers. remaining fuel: {CurrentFuel} Litr.");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough fuel! Try traveling less distance.");
+            }
+        }
+    }
+
+}
+
---
+++ Task 2 05.21.2024/Task 2 05.21.2024/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_2_05._21._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Car car = new Car("Mercedes", "W210", 50.0, 0.6);
+
+                car.Drive(30);
+                car.Drive(50);
+                car.Drive(10);
+
+            }
+        }
+    }
+}
+
