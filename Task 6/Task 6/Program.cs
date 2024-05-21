8614495dff1fdb4d27dbc5b0afd5bc0568f71f7c
AliShiklinski <169164888+AliShiklinski@users.noreply.github.com>
2024-05-21 10:18:16 +0400

    Add files via upload

---
+++ Task 6/Task 6/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "q 5 8 @ 2 m %";
+
+            Console.WriteLine(password.Length);
+
+            Console.WriteLine(password.Contains("q"));
+
+
+        }
+    }
+}
+
