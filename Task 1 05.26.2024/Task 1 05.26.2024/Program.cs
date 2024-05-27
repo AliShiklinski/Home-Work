975f977f4b54396b763bbd2e7541f5ebccd83a12
AliShiklinski <169164888+AliShiklinski@users.noreply.github.com>
2024-05-27 10:37:06 +0400

    Add files via upload

---
+++ Task 1 05.26.2024/Task 1 05.26.2024/Program.cs
@@ -0,0 +1,22 @@
+﻿using Task_1_05._26._2024;
+
+namespace Task_1_05._26._2024
+{
+    internal class Program
+    {
+ 
+    public static void Main()
+        {
+            User user = new User();
+
+            Console.Write("Enter your name: ");
+            user.Name = Console.ReadLine();
+
+            Console.Write("Enter your password: ");
+            user.Password = Console.ReadLine();
+        }
+    }
+}
+
+
+
---
+++ Task 1 05.26.2024/Task 1 05.26.2024/User.cs
@@ -0,0 +1,51 @@
+﻿namespace Task_1_05._26._2024
+{
+    internal class User
+    {
+        
+        private string name;
+        private string password;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if (IsValidPassword(value))
+                {
+                    password = value;
+                    Console.WriteLine("Password is valid.");
+                }
+                else
+                {
+                    Console.WriteLine("Password is invalid.");
+                }
+            }
+        }
+
+        private bool IsValidPassword(string password)
+        {
+            if (password.Length <= 7)
+                return false;
+
+            bool hasUpper = false, hasLower = false, hasSpecial = false, hasNumber = false;
+
+            foreach (char c in password)
+            {
+                if (char.IsUpper(c)) hasUpper = true;
+                else if (char.IsLower(c)) hasLower = true;
+                else if (char.IsDigit(c)) hasNumber = true;
+                else if (char.IsPunctuation(c) || char.IsSymbol(c)) hasSpecial = true;
+            }
+
+            return hasUpper && hasLower && hasSpecial && hasNumber;
+        }
+    }
+}
+
