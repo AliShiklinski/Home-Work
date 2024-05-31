b8d5bdee6b70ee0213c84339dfa4f54e4afe5b5d
AliShiklinski <169164888+AliShiklinski@users.noreply.github.com>
2024-05-31 15:48:07 +0400

    Add files via upload

---
+++ Task 2 05.31.2024/Enum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public enum CarType
+{
+    Sport,
+    SUV,
+    Sedan
+}
+
+public class Car
+{
+    private static int nextId = 1;
+
+    public int Id { get; }
+    public string Brand { get; }
+    public string Model { get; }
+    public CarType Type { get; }
+
+    public Car(string brand, string model, CarType type)
+    {
+        Id = nextId++;
+        Brand = brand;
+        Model = model;
+        Type = type;
+    }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, Brand: {Brand}, Model: {Model}, Type: {Type}";
+    }
+}
