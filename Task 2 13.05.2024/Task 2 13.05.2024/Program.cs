29d0b0d545ff1200e368e65075bb4d19b83310bb
AliShiklinski <169164888+AliShiklinski@users.noreply.github.com>
2024-06-06 15:08:10 +0400

    Add files via upload

---
+++ Task 2 13.05.2024/Task 2 13.05.2024/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+  public class Program
+{
+    static void Main()
+    {
+
+        int n = 23452;
+        Console.WriteLine("The number of floors is: " + CountFloors(n));
+
+        n = 456;
+        Console.WriteLine("The number of floors is: " + CountFloors(n));
+    }
+
+    static int CountFloors(int n)
+    {
+
+        string numberStr = n.ToString();
+
+
+        return numberStr.Length;
+    }
+}
