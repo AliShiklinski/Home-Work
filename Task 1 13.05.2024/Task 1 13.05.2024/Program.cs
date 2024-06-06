a95b2d33012946a9c7af54ee4a47d8ac7872fe82
AliShiklinski <169164888+AliShiklinski@users.noreply.github.com>
2024-06-06 15:06:03 +0400

    Add files via upload

---
+++ Task 1 13.05.2024/Task 1 13.05.2024/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+ public class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter an integer: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (IsPrime(n))
+        {
+            Console.WriteLine($"{n} is a prime number.");
+        }
+        else
+        {
+            Console.WriteLine($"{n} is a composite number.");
+        }
+    }
+
+    static bool IsPrime(int n)
+    {
+        if (n <= 1)
+        {
+            return false;
+        }
+        if (n <= 3)
+        {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0)
+        {
+            return false;
+        }
+
+        for (int i = 5; i * i <= n; i += 6)
+        {
+            if (n % i == 0 || n % (i + 2) == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
