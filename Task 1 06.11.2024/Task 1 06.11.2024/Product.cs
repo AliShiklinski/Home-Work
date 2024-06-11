51b6ebfe2c874ddffe5a5f4cd91317495367b54c
AliShiklinski <169164888+AliShiklinski@users.noreply.github.com>
2024-06-11 13:56:33 +0400

    Update Product.cs

--- Task 1 06.11.2024/Task 1 06.11.2024/Product.cs
+++ Task 1 06.11.2024/Task 1 06.11.2024/Product.cs
@@ -4,23 +4,23 @@ using System.Collections.Generic;
 
 public class Product
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public decimal CostPrice { get; set; } = 1500; 
-    public decimal SalePrice { get; set; } = 2500; 
+    public int Id { get; INIT; }
+    public string Name { get; INIT; }
+    public decimal CostPrice { get; INIT; } = 1500; 
+    public decimal SalePrice { get; INIT; } = 2500; 
 }
 
 
 public class ProductGetDto
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public decimal SalePrice { get; set; }
+    public int Id { get; INIT; }
+    public string Name { get; INIT; }
+    public decimal SalePrice { get; INIT; }
 }
 
 public class ProductCreateDto
 {
-    public string Name { get; set; }
-    public decimal CostPrice { get; set; }
-    public decimal SalePrice { get; set; }
+    public string Name { get; INIT; }
+    public decimal CostPrice { get; INIT; }
+    public decimal SalePrice { get; INIT; }
 }
