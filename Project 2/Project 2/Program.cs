0d55a6ddb1b6b53bfb663ae16c22b764851b547e
AliShiklinski <169164888+AliShiklinski@users.noreply.github.com>
2024-06-19 11:28:37 +0400

    Add files via upload

---
+++ Project 2/Project 2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Sarjor hecmini daxil edin:");
+        int magazineCapacity = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Cari gullelerin sayını daxil edin:");
+        int currentBullets = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ates rejiminə daxil olun (Tek/Avtomatik):");
+        string fireMode = Console.ReadLine();
+
+        Weapon weapon = new Weapon(magazineCapacity, currentBullets, fireMode);
+
+        while (true)
+        {
+            Console.WriteLine("0 - Get Information");
+            Console.WriteLine("1 - Shoot");
+            Console.WriteLine("2 - Fire");
+            Console.WriteLine("3 - Get Remain Bullet Count");
+            Console.WriteLine("4 - Reload");
+            Console.WriteLine("5 - Change Fire Mode");
+            Console.WriteLine("6 - Quit");
+            Console.WriteLine("7 - Edit");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine($"Magazine Capacity: {weapon.MagazineCapacity}, Current Bullets: {weapon.GetRemainBulletCount()}, Fire Mode: {weapon.FireMode}");
+                    break;
+                case 1:
+                    weapon.Shoot();
+                    break;
+                case 2:
+                    weapon.Fire();
+                    break;
+                case 3:
+                    Console.WriteLine("Qalan gülleler: " + weapon.GetRemainBulletCount());
+                    break;
+                case 4:
+                    weapon.Reload();
+                    break;
+                case 5:
+                    weapon.ChangeFireMode();
+                    break;
+                case 6:
+                    return;
+                case 7:
+                    Console.WriteLine("1 - Jurnal tutumunu deyisdirin");
+                    Console.WriteLine("2 - Cari gulleleri deyisdirin");
+
+                    int editChoice = int.Parse(Console.ReadLine());
+
+                    if (editChoice == 1)
+                    {
+                        Console.WriteLine("Yeni sarjor tutumunu daxil edin:");
+                        int newCapacity = int.Parse(Console.ReadLine());
+                        weapon.EditMagazineCapacity(newCapacity);
+                    }
+                    else if (editChoice == 2)
+                    {
+                        Console.WriteLine("Yeni cari güllelerin sayını daxil edin:");
+                        int newBulletCount = int.Parse(Console.ReadLine());
+                        weapon.EditCurrentBullets(newBulletCount);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Yanlıs secim. Zehmet olmasa bir daha cehd edin.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Project 2/Project 2/Weapon.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+public class Weapon
+{
+    public int MagazineCapacity { get; private set; }
+    public int CurrentBullets { get; private set; }
+    public string FireMode { get; private set; }
+
+    public Weapon(int magazineCapacity, int currentBullets, string fireMode)
+    {
+        if (magazineCapacity <= 0 || currentBullets < 0 || string.IsNullOrEmpty(fireMode))
+        {
+            throw new ArgumentException("Invalid arguments for Weapon creation.");
+        }
+
+        MagazineCapacity = magazineCapacity;
+        CurrentBullets = currentBullets;
+        FireMode = fireMode;
+    }
+
+    public void Shoot()
+    {
+        if (CurrentBullets > 0)
+        {
+            CurrentBullets--;
+            Console.WriteLine("Bir gulle atıldı. Qalan gulleler: " + CurrentBullets);
+        }
+        else
+        {
+            Console.WriteLine("Atılmaga gulle qalmadı.");
+        }
+    }
+
+    public void Fire()
+    {
+        if (CurrentBullets > 0)
+        {
+            Console.WriteLine(CurrentBullets + " gulleler atıldı.");
+            CurrentBullets = 0;
+        }
+        else
+        {
+            Console.WriteLine("Ates ucun gulle qalmadı.");
+        }
+    }
+
+    public int GetRemainBulletCount()
+    {
+        return CurrentBullets;
+    }
+
+    public void Reload()
+    {
+        CurrentBullets = MagazineCapacity;
+        Console.WriteLine("Magazine reloaded. Bullets: " + CurrentBullets);
+    }
+
+    public void ChangeFireMode()
+    {
+        FireMode = FireMode == "Tek" ? "Automatic" : "Tek";
+        Console.WriteLine("Fire mode changed to: " + FireMode);
+    }
+
+    public void EditMagazineCapacity(int newCapacity)
+    {
+        if (newCapacity > 0)
+        {
+            MagazineCapacity = newCapacity;
+            Console.WriteLine("Magazine capacity changed to: " + MagazineCapacity);
+        }
+        else
+        {
+            Console.WriteLine("Invalid magazine capacity.");
+        }
+    }
+
+    public void EditCurrentBullets(int newBulletCount)
+    {
+        if (newBulletCount >= 0 && newBulletCount <= MagazineCapacity)
+        {
+            CurrentBullets = newBulletCount;
+            Console.WriteLine("Current bullets count changed to: " + CurrentBullets);
+        }
+        else
+        {
+            Console.WriteLine("Invalid bullet count.");
+        }
+    }
+}
