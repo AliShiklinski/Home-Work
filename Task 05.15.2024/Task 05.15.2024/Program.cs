db21fd71f84524de59f16e9a0f4a34b4c4449759
AliShiklinski <169164888+AliShiklinski@users.noreply.github.com>
2024-07-19 15:31:22 +0400

    Add files via upload

---
+++ Task 05.15.2024/Task 05.15.2024/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] sayilar = { 10, 20, 5, 25, 8, 30 };
+
+        int enBoyuk = EnBuyukSayiyiBul(sayilar);
+
+        Console.WriteLine("En boyuk sayı: " + enBoyuk);
+    }
+
+    static int EnBuyukSayiyiBul(int[] sayilar)
+    {
+        int enBoyuk = sayilar[0];
+
+        foreach (int sayi in sayilar)
+        {
+            if (sayi > enBoyuk)
+            {
+                enBoyuk = sayi;
+            }
+        }
+
+        return enBoyuk;
+    }
+}
